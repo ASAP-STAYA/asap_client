5bbb299f009e46443615622f8fb95ef8a9e9d4b2
sen <seeunchoi99@gmail.com>
2022-10-02 22:14:01 +0900

    선호도 - 거리, 요금 추가

--- lib/screen/screen_sign_up.dart
+++ lib/screen/screen_sign_up.dart
@@ -13,6 +13,11 @@ class SignUpScreen extends StatefulWidget {
 }
 
 class _SignUpScreen extends State<SignUpScreen> {
+  late Size screenSize;
+  late double width;
+
+  late double height;
+
   final _nameController = TextEditingController();
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
@@ -26,12 +31,14 @@ class _SignUpScreen extends State<SignUpScreen> {
 
   late List<bool> isSelectedMechanical = [false, true];
   late List<bool> isSelectedSmall = [false, true];
+  late List<bool> isSelectedDistance = [false, false, false, true];
+  late List<bool> isSelectedPrice = [false, false, false, true];
 
   @override
   Widget build(BuildContext context) {
-    Size screenSize = MediaQuery.of(context).size;
-    double width = screenSize.width;
-    double height = screenSize.height;
+    screenSize = MediaQuery.of(context).size;
+    width = screenSize.width;
+    height = screenSize.height;
 
     _userProvider = Provider.of<UserProvider>(context);
 
@@ -79,8 +86,7 @@ class _SignUpScreen extends State<SignUpScreen> {
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
-          title: Text('SIGN UP'),
-          // leading: Container(),
+          title: Text('회원가입'),
         ),
         body: Container(
           child: ListView(
@@ -116,16 +122,32 @@ class _SignUpScreen extends State<SignUpScreen> {
                 children: <Widget>[
                   Container(
                     margin: EdgeInsets.fromLTRB(40, 30, 40, 10),
-                    child: _inputPreference("기계식 주차장"),
+                    child: _inputPrefer1("기계식 주차장"),
                   ),
                   Container(
                     margin: EdgeInsets.fromLTRB(40, 0, 40, 0),
-                    child: _inputPreference("좁은 주차장"),
+                    child: _inputPrefer1("좁은 주차장"),
                   )
                 ],
               ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.fromLTRB(0, height * 0.03, 0, 0),
+                    child: _inputPrefer2(
+                        "거리", "~0.5km", "~1km", "~1.5km", "상관 없어"),
+                  ),
+                  Container(
+                    margin: EdgeInsets.fromLTRB(0, height * 0.02, 0, height * 0.05),
+                    child:
+                        _inputPrefer2("요금", "무료만", "~500원", "~1000원", "상관 없어"),
+                  ),
+                ],
+              ),
               ElevatedButton(
-                style: ElevatedButton.styleFrom(padding: EdgeInsets.symmetric(vertical: height * 0.015)),
+                style: ElevatedButton.styleFrom(
+                    padding: EdgeInsets.symmetric(vertical: height * 0.015)),
                 onPressed: () => _checkValidation(),
                 child: const Text(
                   '가입하기',
@@ -139,22 +161,22 @@ class _SignUpScreen extends State<SignUpScreen> {
     );
   }
 
-  Widget _inputPreference(String string) {
+  Widget _inputPrefer1(String type) {
     List<bool> isSelected;
 
-    if ("기계식 주차장" == string) {
+    if ("기계식 주차장" == type) {
       isSelected = isSelectedMechanical;
-    } else if ("좁은 주차장" == string) {
+    } else if ("좁은 주차장" == type) {
       isSelected = isSelectedSmall;
     } else {
-      throw Exception("[ERROR] Invalid argument in _inputPreference");
+      throw Exception("[ERROR] Invalid argument in _inputPrefer1");
     }
 
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
         Text(
-          string,
+          type,
           style: const TextStyle(
               fontSize: 18, fontFeatures: [FontFeature.tabularFigures()]),
         ),
@@ -172,7 +194,7 @@ class _SignUpScreen extends State<SignUpScreen> {
               });
             },
             borderRadius: BorderRadius.circular(10),
-            constraints: BoxConstraints(minHeight: 35, maxHeight: 500),
+            constraints: BoxConstraints(minHeight: 35),
             children: const [
               Padding(
                 padding: EdgeInsets.symmetric(horizontal: 10),
@@ -192,12 +214,90 @@ class _SignUpScreen extends State<SignUpScreen> {
       ],
     );
   }
+
+  Widget _inputPrefer2(
+      String type, String arg1, String arg2, String arg3, String arg4) {
+    List<bool> isSelected;
+
+    if ("거리" == type) {
+      isSelected = isSelectedDistance;
+    } else if ("요금" == type) {
+      isSelected = isSelectedPrice;
+    } else {
+      throw Exception("[ERROR] Invalid argument in _inputPrefer2");
+    }
+
+    return Column(
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.only(bottom: 5),
+          child: Text(
+            type,
+            style: const TextStyle(fontSize: 18),
+          ),
+        ),
+        ToggleButtons(
+            isSelected: isSelected,
+            onPressed: (int index) {
+              setState(
+                () {
+                  if (index == 0) {
+                    isSelected[0] = true;
+                    isSelected[1] = isSelected[2] = isSelected[3] = false;
+                  } else if (index == 1) {
+                    isSelected[1] = true;
+                    isSelected[0] = isSelected[2] = isSelected[3] = false;
+                  } else if (index == 2) {
+                    isSelected[2] = true;
+                    isSelected[1] = isSelected[0] = isSelected[3] = false;
+                  } else {
+                    isSelected[3] = true;
+                    isSelected[1] = isSelected[2] = isSelected[0] = false;
+                  }
+                },
+              );
+            },
+            borderRadius: BorderRadius.circular(9),
+            constraints: BoxConstraints(minHeight: 30, minWidth: width * 0.2),
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                child: Text(
+                  arg1,
+                  style: TextStyle(fontSize: 16),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                child: Text(
+                  arg2,
+                  style: TextStyle(fontSize: 16),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                child: Text(
+                  arg3,
+                  style: TextStyle(fontSize: 16),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                child: Text(
+                  arg4,
+                  style: TextStyle(fontSize: 16),
+                ),
+              ),
+            ]),
+      ],
+    );
+  }
 }
 
-Widget _inputForm(String name, TextEditingController textEditingController,
+Widget _inputForm(String type, TextEditingController textEditingController,
     String errorMsg, double width) {
   final TextFormField textFormField;
-  if ("이메일" == name) {
+  if ("이메일" == type) {
     textFormField = TextFormField(
       keyboardType: TextInputType.emailAddress,
       controller: textEditingController,
@@ -208,12 +308,12 @@ Widget _inputForm(String name, TextEditingController textEditingController,
         ),
       ),
     );
-  } else if ("이름" == name) {
+  } else if ("이름" == type) {
     textFormField = TextFormField(
       keyboardType: TextInputType.name,
       controller: textEditingController,
     );
-  } else if ("비밀번호" == name || "비밀번호 확인" == name) {
+  } else if ("비밀번호" == type || "비밀번호 확인" == type) {
     textFormField = TextFormField(
       obscureText: true,
       keyboardType: TextInputType.visiblePassword,
@@ -233,7 +333,7 @@ Widget _inputForm(String name, TextEditingController textEditingController,
     mainAxisAlignment: MainAxisAlignment.spaceBetween,
     children: [
       Text(
-        name,
+        type,
         style: const TextStyle(
             fontSize: 18, fontFeatures: [FontFeature.tabularFigures()]),
       ),
