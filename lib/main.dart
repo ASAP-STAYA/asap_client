74f4021c464ab4e2a6c5e6a0e873f175aa1117b8
sen <seeunchoi99@gmail.com>
2022-10-10 14:51:22 +0900

    메인화면 구현

--- lib/main.dart
+++ lib/main.dart
@@ -12,19 +12,18 @@ import 'package:kakao_flutter_sdk_common/kakao_flutter_sdk_common.dart';
 import 'package:kakao_flutter_sdk/kakao_flutter_sdk_navi.dart';
 import 'package:provider/provider.dart';
 
-
 /* http 통신 */
 Future<User> fetchUser() async {
   final httpurl = 'https://jsonplaceholder.typicode.com/posts/1';
   final response = await http.get(Uri.parse(httpurl));
 
-  if (response.statusCode == 200){ // 서버로의 요청 성공 -> JSON 파싱
+  if (response.statusCode == 200) {
+    // 서버로의 요청 성공 -> JSON 파싱
     return User.fromMap(json.decode(response.body));
-  }
-  else{ // 요청 실패 -> throw error
+  } else {
+    // 요청 실패 -> throw error
     throw Exception('Failed to load post');
   }
-
 }
 
 void main() async {
@@ -38,7 +37,6 @@ void main() async {
     // 웹
     //isJavaScriptAppKey: '${YOUR_JAVASCRIPT_APP_KEY}',
   );
-
 
   // 카카오 API 연동
 
@@ -59,11 +57,9 @@ void main() async {
   print('카카오 내비 끝');
 */
 
-  runApp(MultiProvider(
-      providers: [
-        ListenableProvider(create: (_) => UserProvider()),
-      ],
-      child: MyApp()));
+  runApp(MultiProvider(providers: [
+    ListenableProvider(create: (_) => UserProvider()),
+  ], child: MyApp()));
 
   // Backend와 연동
 /*
@@ -72,8 +68,6 @@ void main() async {
   print('Response status: ${response.statusCode}');
   print('Response body: ${response.body}');
 */
-
-
 }
 
 class MyApp extends StatelessWidget {
@@ -83,20 +77,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'ASAP',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'ASAP'),
       // home: SignUpScreen(),
     );
   }
@@ -105,15 +90,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -121,7 +97,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
   Future<User>? user;
   late Size screenSize;
   late double width;
@@ -133,84 +108,38 @@ class _MyHomePageState extends State<MyHomePage> {
     user = fetchUser();
   }
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    screenSize = MediaQuery
-        .of(context)
-        .size;
+    screenSize = MediaQuery.of(context).size;
     width = screenSize.width;
     height = screenSize.height;
 
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            Image.asset('images/playstore.png', width: width * 0.7,),
             ElevatedButton(
               onPressed: () => Navigator.push(context,
                   MaterialPageRoute(builder: (context) => SignUpScreen())),
-              child: Text('회원가입') ,
-
+              child: Text('회원가입'),
             ),
             Padding(
-              padding: EdgeInsets.all(width*0.025),
+              padding: EdgeInsets.all(width * 0.025),
             ),
             ElevatedButton(
               onPressed: () => Navigator.push(context,
                   MaterialPageRoute(builder: (context) => SelectScreen())),
-              child: Text('내비 안내') ,
-
+              child: Text('내비 안내'),
             ),
             FutureBuilder<User>(
               future: user,
               builder: (context, snapshot) {
-                if(snapshot.hasData) {
+                if (snapshot.hasData) {
                   return Text("타이틀 :  ${snapshot.data!.title}");
                 } else if (snapshot.hasError) {
                   return Text("에러가 발생했습니다\n ${snapshot.error}");
@@ -221,11 +150,6 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
