0dcd7b29770d504f9b46ecde76753fa54150f125
sen <seeunchoi99@gmail.com>
2022-11-21 04:23:40 +0900

    feat: add functions of util_cost and util_distance

--- lib/util/util_distance.dart
+++ lib/util/util_distance.dart
@@ -1,4 +1,4 @@
-const List<double> distance = [0.5, 1.0, 2.0, -1.0];
+const List<double> distances = [0.5, 1.0, 2.0, -1.0];
 
 String distanceToString(double distance) {
   if (distance == -1.0) {
@@ -6,4 +6,17 @@ String distanceToString(double distance) {
   } else {
     return "~${distance}km";
   }
+}
+
+int getIndexOfDistance(double distance) {
+  return distances.indexOf(distance);
+}
+
+double getDistanceFromSelectedList(List<bool> isSelected) {
+  for (int i = 0; i < isSelected.length; i++) {
+    if (isSelected[i] == true) {
+      return distances[i];
+    }
+  }
+  throw Exception("[ERROR] Any distance is not selected");
 }
