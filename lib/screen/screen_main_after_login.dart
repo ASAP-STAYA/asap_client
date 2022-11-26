31f10012cef7e7e44c8351af273aea66ad64b637
serajang99 <serajang1679@gmail.com>
2022-11-27 01:45:39 +0900

    fix: main after login back button 수정

--- lib/screen/screen_main_after_login.dart
+++ lib/screen/screen_main_after_login.dart
@@ -1,6 +1,7 @@
 import 'package:asap_client/screen/screen_setting.dart';
 import 'package:asap_client/screen/screen_voice1.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class MainAfterLoginScreen extends StatelessWidget {
   late Size screenSize;
@@ -16,46 +17,52 @@ class MainAfterLoginScreen extends StatelessWidget {
     height = screenSize.height;
 
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('ASAP',
-                style: TextStyle(
-                  fontFamily: 'EliceDigitalBaeum_TTF',
-                  fontSize: 80.0,
-                  color: Color(0xff0f4c81),
-                  fontWeight: FontWeight.w700,
-                )),
-            Padding(
-              padding: EdgeInsets.all(width * 0.105),
-            ),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                primary: const Color(0xff0f4c81),
-                minimumSize: const Size(150, 50),
-              ),
-              onPressed: () => Navigator.push(
-                  context, MaterialPageRoute(builder: (context) => SpeechScreen())),
-              child: const Text('안내 시작',
+      body:WillPopScope(
+        onWillPop: () async {
+        SystemNavigator.pop();
+        return false;
+        },
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text('ASAP',
                   style: TextStyle(
-                      fontFamily: 'EliceDigitalBaeum_TTF', fontSize: 20.0)),
-            ),
-            Padding(
-              padding: EdgeInsets.all(width * 0.010),
-            ),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                primary: const Color(0xff0f4c81),
-                minimumSize: const Size(150, 50),
+                    fontFamily: 'EliceDigitalBaeum_TTF',
+                    fontSize: 80.0,
+                    color: Color(0xff0f4c81),
+                    fontWeight: FontWeight.w700,
+                  )),
+              Padding(
+                padding: EdgeInsets.all(width * 0.105),
               ),
-              onPressed: () => Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => SettingScreen())),
-              child: const Text('설정',
-                  style: TextStyle(
-                      fontFamily: 'EliceDigitalBaeum_TTF', fontSize: 20.0)),
-            ),
-          ],
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: const Color(0xff0f4c81),
+                  minimumSize: const Size(150, 50),
+                ),
+                onPressed: () => Navigator.push(
+                    context, MaterialPageRoute(builder: (context) => SpeechScreen())),
+                child: const Text('안내 시작',
+                    style: TextStyle(
+                        fontFamily: 'EliceDigitalBaeum_TTF', fontSize: 20.0)),
+              ),
+              Padding(
+                padding: EdgeInsets.all(width * 0.010),
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: const Color(0xff0f4c81),
+                  minimumSize: const Size(150, 50),
+                ),
+                onPressed: () => Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => SettingScreen())),
+                child: const Text('설정',
+                    style: TextStyle(
+                        fontFamily: 'EliceDigitalBaeum_TTF', fontSize: 20.0)),
+              ),
+            ],
+          ),
         ),
       ),
     );
